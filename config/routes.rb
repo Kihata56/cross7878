ca243f756e3f0855b37228456e57508d6cfe0467
Kihata Syunpei <paypay569899@gmail.com>
2020-07-15 18:34:05 +0900

    tys

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users,
+    controllers: { registrations: 'registrations' }
+
   root 'pages#home'
+
+  get '/users/:id', to: 'users#show', as: 'user'
+
+  # ==========ここから編集する==========
+  resources :posts, only: %i(new create) do
+    resources :photos, only: %i(create)
+  end
+  # ==========ここまで編集する==========
+
 end
+
